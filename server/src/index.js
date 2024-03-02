5785816ce4b34db6bdd98ffc7d83634e0f88ce54
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-03-02 10:12:34 +0530

    Add express server

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/db.js";
 import dotenv from "dotenv";
 
@@ -6,3 +7,17 @@ dotenv.config({
 });
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        });
+
+        app.listen(process.env.PORT || 8080, () => {
+            console.log(`Server is running PORT: , ${process.env.PORT}`);
+        });
+
+    })
+    .catch((error) => {
+        console.log("MongoDB Connection Failed ", error);
+    });
