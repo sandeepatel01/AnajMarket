efba6c89ed76fa5f3c696388494804c2c9d92d4b
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-03-23 20:09:59 +0530

    Add user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+            unique: true,
+            lowercase: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+            unique: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        avatar: {
+            type: String,
+            required: true
+        },
+        coverImage: {
+            type: String
+        }
+    },
+    { timestamps: true }
+);
+
+
+export const User = mongoose.model("User", userSchema);
